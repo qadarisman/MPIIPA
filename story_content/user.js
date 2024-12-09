a8f07c174557abf56ced5d5bef4b070c21b8a2cf
qadarisman <ahmadrisman746@gmail.com>
2024-12-09 14:55:03 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,242 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ZTWcMQiNhd":
+        Script1();
+        break;
+      case "6jz283HMqQO":
+        Script2();
+        break;
+      case "6WgFEh9N9rf":
+        Script3();
+        break;
+      case "5X4d7dHWMUU":
+        Script4();
+        break;
+      case "5kAlCp9QS5n":
+        Script5();
+        break;
+      case "6mFdklT7LTd":
+        Script6();
+        break;
+      case "5vrJLNITNmU":
+        Script7();
+        break;
+      case "63uzC1U57qO":
+        Script8();
+        break;
+      case "6N5aaPqcJdm":
+        Script9();
+        break;
+      case "5ronGh6D9wE":
+        Script10();
+        break;
+      case "5z2zogzNkXW":
+        Script11();
+        break;
+      case "6WuCTQ0qzH6":
+        Script12();
+        break;
+      case "5vSlgHVHLmn":
+        Script13();
+        break;
+      case "6ow8TIZHQlj":
+        Script14();
+        break;
+      case "5V7EHpGYS5D":
+        Script15();
+        break;
+      case "6YV1zlvJ87y":
+        Script16();
+        break;
+      case "5hYgnPBb5B4":
+        Script17();
+        break;
+      case "6JU7lWDR6iO":
+        Script18();
+        break;
+      case "6d0wim28YQw":
+        Script19();
+        break;
+      case "5WsmUBIVIRS":
+        Script20();
+        break;
+      case "6aDgV8t1Bx8":
+        Script21();
+        break;
+      case "6OhIEmuqYaN":
+        Script22();
+        break;
+      case "6bLXxcMbldx":
+        Script23();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.3;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.8;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.9;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 1.0;
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.0;
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.1;
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.2;
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.3;
+}
+
+function Script17()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.4;
+}
+
+function Script18()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.5;
+}
+
+function Script19()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.6;
+}
+
+function Script20()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.7;
+}
+
+function Script21()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.8;
+}
+
+function Script22()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 0.9;
+}
+
+function Script23()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.volume = 1.0;
+}
+
